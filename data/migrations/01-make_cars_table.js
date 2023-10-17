a70c3461710b03ebe7230afef16580da902d9aa6
Zack Honey <zhoney466@gmail.com>
2023-10-17 12:53:24 -0700

    building schema

--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -1,5 +1,12 @@
 exports.up = function (knex) {
   // DO YOUR MAGIC
+  return knex.schema.createTable('cars', tbl => {
+    tbl.increments()
+
+    tbl.string('vin', 17).notNullable().unique()
+
+    tbl.string('make', 123).notNullable()
+  })
 };
 
 exports.down = function (knex) {
--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,31 @@
 // STRETCH
+const cars = [
+    {
+        vin: '1111111111111',
+        make: 'Honda',
+        model: 'Civic',
+        mileage: 67500,
+        title: 'clean',
+        trasmission: 'manual'
+    },
+    {
+        vin: '1111111111112',
+        make: 'Honda',
+        model: 'Accord',
+        mileage: 88500,
+        title: 'salvage',
+        trasmission: 'automatic'
+    },
+    {
+        vin: '1111111111113',
+        make: 'GMC',
+        model: 'Seirra',
+        mileage: 210000,
+
+    }
+]
+
+exports.seed = async function (knex) {
+    await knex('cars').truncate()
+    await knex('cars').insert(cars)
+}
