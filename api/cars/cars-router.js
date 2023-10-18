194254d8c003cd80e40829ca92715a0343d824d5
Zack Honey <zhoney466@gmail.com>
2023-10-17 17:11:25 -0700

    wip

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,6 +1,13 @@
 // DO YOUR MAGIC
 const express = require('express')
 const Car = require('./cars-model')
+const {
+checkCarId,
+checkCarPayload,
+checkVinNumberUnique,
+checkVinNumberValid
+
+} = require('./cars-middleware')
 const router = express.Router()
 
 router.get('/', async (req, res, next) => {
@@ -12,13 +19,8 @@ router.get('/', async (req, res, next) => {
     }
 })
 
-router.get('/:id', async (req, res, next) => {
-    try {
-        const car = await Car.getById(req.params.id)
-        res.json(car)
-    } catch (err) {
-        next(err)
-    }
+router.get('/:id',checkCarId, async (req, res, next) => {
+    res.json(req.car)
 })
 
 router.post('/', async (req, res, next) => {
